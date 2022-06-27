094346094dd01dacd7d2221b6885ca438299cf71
Olivia <ramsfol@gmail.com>
2022-06-27 12:40:52 -0500

    color-coded time slots based on current time

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,3 +5,27 @@ $("#currentDay").innerHTML = todaysDate;
 
 //append current date to <p> element
 $("<p>" + todaysDate + "</p>").appendTo("#currentDay");
+
+/* Begin color coding based on current time */
+
+//get current hour using moment.js
+var currentHour = moment().hour();
+console.log(currentHour);
+
+//color code based on hour
+// var colorcode = document.getElementsByClassName("colorcode");
+$('.colorcode').each(function(){
+    var hour = parseInt($(this).prop('id'));
+    if (hour > currentHour){
+        $(this).removeClass("present past").addClass("future");
+    }
+    else if (hour < currentHour){
+        $(this).removeClass("future present").addClass("past");
+    }
+    else if (hour == currentHour) {
+        $(this).removeClass("future past").addClass("present");
+    }
+});
+
+// setInterval(checkTime, 1000);
+// console.log(checkTime);
